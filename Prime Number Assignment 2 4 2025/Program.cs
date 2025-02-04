bf434debf6144959428667a3908f364a8877b8a0
Ashton Pate <arpate@amarillocollege.com>
2025-02-04 13:42:46 -0600

    worte some code

--- Prime Number Assignment 2 4 2025/Program.cs
+++ Prime Number Assignment 2 4 2025/Program.cs
@@ -18,7 +18,12 @@ namespace Prime_Number_Assignment_2_4_2025
 
         static void Main(string[] args)
         {
+            double usernumber =double.MaxValue;
+
             //promt user to input an number
+            Console.WriteLine("input an number");
+
+            usernumber = PrimeNumberCheck();
 
         }
     }
