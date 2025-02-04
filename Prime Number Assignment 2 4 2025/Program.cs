3e9fc9ddae0bd236b7f2473aaeef52b3ce90a942
Ashton Pate <arpate@amarillocollege.com>
2025-02-04 13:41:16 -0600

    Add project files.

---
+++ Prime Number Assignment 2 4 2025/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number_Assignment_2_4_2025
+{
+    internal class Program
+    {
+        static double PrimeNumberCheck()
+        {
+            for
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //promt user to input an number
+
+        }
+    }
+}
